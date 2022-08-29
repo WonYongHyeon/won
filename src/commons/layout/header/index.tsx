44badefda18a500d096517a04bc52afd5050c30f
WonYongHyeon <quin1392@gmail.com>
2022-08-29 10:21:09 +0900

    8/29 ItemDetailComment

--- src/commons/layout/header/index.tsx
+++ src/commons/layout/header/index.tsx
@@ -4,18 +4,23 @@ import { useRouter } from "next/router";
 import { useSetRecoilState } from "recoil";
 import Swal from "sweetalert2";
 import { AccessToken } from "../../store";
+import "animate.css";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   height: 100px;
   background-color: black;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
 const HeaderEnableArea = styled.div`
+  position: fixed;
+  top: 0;
   width: 80%;
-  height: 100%;
+  /* height: 100%; */
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -68,6 +73,9 @@ export default function LayoutHeader() {
   const setAccessToken = useSetRecoilState(AccessToken);
   const { data } = useQuery(FETCH_USER_LOGGED_IN);
   const [logoutUser] = useMutation(LOGOUT_USER);
+  const [isMenu, setIsMenu] = useState(false);
+
+  const [count, setCount] = useState(0);
 
   const onClickLogo = () => {
     router.push("/");
@@ -94,21 +102,43 @@ export default function LayoutHeader() {
     }
   };
 
+  // const onHoverOver = () => {
+  //   // if (count === 0) {
+  //   // setCount(1);
+  //   setIsMenu(true);
+  //   // }
+  //   console.log("1");
+  // };
+  // const onHoverOut = () => {
+  //   // setCount(0);
+  //   setIsMenu(false);
+  // };
+
   return (
-    <>
-      <Wrapper>
-        <HeaderEnableArea>
-          <LogoText onClick={onClickLogo}>공구</LogoText>
-          <MenuWrapper>
-            <MenuText onClick={data ? undefined : onClickLogin}>
-              {data ? data.fetchUserLoggedIn.name + " 님" : "로그인"}
-            </MenuText>
-            <MenuText onClick={data ? onClickLogout : onClickSignup}>
-              {data ? "로그아웃" : "회원가입"}
-            </MenuText>
-          </MenuWrapper>
-        </HeaderEnableArea>
-      </Wrapper>
-    </>
+    <Wrapper
+    // onMouseOver={onHoverOver} onMouseOut={onHoverOut}
+    >
+      <HeaderEnableArea>
+        <LogoText onClick={onClickLogo}>공구</LogoText>
+        <MenuWrapper>
+          <MenuText onClick={data ? undefined : onClickLogin}>
+            {data ? data.fetchUserLoggedIn.name + " 님" : "로그인"}
+          </MenuText>
+          <MenuText onClick={data ? onClickLogout : onClickSignup}>
+            {data ? "로그아웃" : "회원가입"}
+          </MenuText>
+        </MenuWrapper>
+      </HeaderEnableArea>
+      {/* {isMenu && (
+        // <div style={{ height: "100px", marginTop: "100px" }}>
+        <div
+          style={{ color: "red", height: "100px", marginTop: "100px" }}
+          className="animate__animated animate__fadeInDown"
+        >
+          ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ
+        </div>
+        // </div>
+      )} */}
+    </Wrapper>
   );
 }
